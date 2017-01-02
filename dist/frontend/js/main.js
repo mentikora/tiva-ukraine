4b61b4de1288b42d8a29b33f85e073cec56325b8
Mentikora <notender_boy@ukr.net>
2017-01-03 01:50:20 +0200

    update

--- dist/frontend/js/main.js
+++ dist/frontend/js/main.js
@@ -2,21 +2,27 @@
 
 $(document).ready(function(){
   
-  //show nav
+  //show nav => theme-def
   $('.mobile_menu_btn').on('click', function(){
     $(this).toggleClass('active');
     ($('#nav').is('.active')) ? $('#nav, #nav .active').removeClass('active') : $('#nav').addClass('active');
   });
 
-  //add active to domtree
+  //add active to domtree => theme-def
   $('#nav span').on('click', function(e){
     e.stopPropagation();
     $(this).toggleClass('active').siblings('ul').toggleClass('active');;
   });
 
-  //show top
+  //show top => theme-def
   $('#header_top_mobile_button').on('click', function(){
     $('#header_top_menu_mobile').toggleClass('active');
+  });
+
+  // categories => filter
+  $('.filter_mobile_button').on('click', function(){
+    $(this).toggleClass('active');
+    $(this).parent('ul').toggleClass('active');
   });
 
 });
