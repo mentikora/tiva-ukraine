47ed7c93b62f11e6dc4930d2cb54e3990c2ca56c
Mentikora <notender_boy@ukr.net>
2017-01-02 13:02:55 +0200

    update

--- dist/frontend/js/main.js
+++ dist/frontend/js/main.js
@@ -1,23 +1,22 @@
 'use strict';
 
 $(document).ready(function(){
-	
-	$('.mobile_menu_btn').on('click', function(){
-		$(this).toggleClass('active');
-		//$('#nav').toggleClass('active');
+  
+  //show nav
+  $('.mobile_menu_btn').on('click', function(){
+    $(this).toggleClass('active');
+    ($('#nav').is('.active')) ? $('#nav, #nav .active').removeClass('active') : $('#nav').addClass('active');
+  });
 
-		if ($('#nav').is('.active')) {
-      $('#nav, #nav .active').removeClass('active');
-	  }
-	  else{
-      $('#nav').addClass('active');
-	  }
-	});
+  //add active to domtree
+  $('#nav span').on('click', function(e){
+    e.stopPropagation();
+    $(this).toggleClass('active').siblings('ul').toggleClass('active');;
+  });
 
-	$('#nav span').on('click', function(e){
-		e.stopPropagation();
-		$(this).toggleClass('active');
-		$(this).siblings('ul').toggleClass('active');
-	});
+  //show top
+  $('#header_top_mobile_button').on('click', function(){
+    $('#header_top_menu_mobile').toggleClass('active');
+  });
 
 });
